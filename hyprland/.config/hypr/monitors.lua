101db5ffa76aff043f7e57df21cec263cebac1bf
Juan Diego <juandiegomariscal@gmail.com>
2026-08-26 15:15:21 +0200

    configuracion mirror

--- hyprland/.config/hypr/monitors.lua
+++ hyprland/.config/hypr/monitors.lua
@@ -4,11 +4,18 @@ local M = {}
 
 function M.setup(hl)
 	hl.monitor({
-		output = "",
+		output = "eDP-1",
 		mode = "preferred",
-		position = "auto",
+		position = "0x0",
 		scale = "auto",
 	})
+
+	-- configuraciones para segundo monitor
+	-- mirror
+	--hl.monitor({ output = "HDMI-A-1", mode = "preferred", position = "0x0", scale = "auto", mirror = "eDP-1" })
+
+	--scaled
+	hl.monitor({ output = "HDMI-A-1", mode = "preferred", position = "0x0", scale = "2.0" })
 end
 
 return M
