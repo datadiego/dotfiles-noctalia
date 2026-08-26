bd31ca365fc9d04be1295dc4dfb40de4dabc8d68
Juan Diego <juandiegomariscal@gmail.com>
2026-08-26 15:15:36 +0200

    reducimos columnas de desplazamiento

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local opt = vim.opt
+
+opt.sidescrolloff = 4  -- Columns of context (reducido de 8)
+opt.sidescroll = 1     -- Columns to scroll horizontally
