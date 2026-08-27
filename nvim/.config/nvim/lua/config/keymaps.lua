9b6b969952c876d34d737c2cc879a73fcfb56808
juandiego <juandiegomariscal@gmail.com>
2026-08-27 16:56:51 +0200

    resize splits

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,21 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local function smart_resize(direction)
+  local current = vim.fn.winnr()
+  local has_right_neighbor = vim.fn.winnr('l') ~= current and vim.fn.winnr('l') > 0
+
+  if not has_right_neighbor then
+    if direction == '>' then
+      direction = '<'
+    elseif direction == '<' then
+      direction = '>'
+    end
+  end
+
+  vim.cmd('wincmd ' .. direction)
+end
+
+vim.keymap.set('n', '<C-Right>', function() smart_resize('>') end, { desc = 'Resize: move separator right' })
+vim.keymap.set('n', '<C-Left>', function() smart_resize('<') end, { desc = 'Resize: move separator left' })
