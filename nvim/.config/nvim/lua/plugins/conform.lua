7d8dd92c792fe7cbae2e5ae080b322a4cb997721
Juan Diego <juandiegomariscal@gmail.com>
2026-08-28 21:34:00 +0200

    quitamos prettier de md

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,16 +1,16 @@
 return {
   "stevearc/conform.nvim",
-  opts = {
-    formatters_by_ft = {
-      javascript = { "prettier" },
-      typescript = { "prettier" },
-      html = { "prettier" },
-      css = { "prettier" },
-      json = { "prettier" },
-      markdown = { "prettier" },
-      python = { "ruff", "black" },
-      lua = { "stylua" },
-      sql = { "sqlfmt" },
-    },
-  },
+  opts = function(_, opts)
+    opts.formatters_by_ft = opts.formatters_by_ft or {}
+    opts.formatters_by_ft.javascript = { "prettier" }
+    opts.formatters_by_ft.typescript = { "prettier" }
+    opts.formatters_by_ft.html = { "prettier" }
+    opts.formatters_by_ft.css = { "prettier" }
+    opts.formatters_by_ft.json = { "prettier" }
+    opts.formatters_by_ft.python = { "ruff", "black" }
+    opts.formatters_by_ft.lua = { "stylua" }
+    opts.formatters_by_ft.sql = { "sqlfmt" }
+    opts.formatters_by_ft.markdown = nil
+    opts.formatters_by_ft["markdown.mdx"] = nil
+  end,
 }
