7a6cae7c2966839a0dd5271764911b92f4e32c56
Juan Diego <juandiegomariscal@gmail.com>
2026-08-28 11:13:29 +0200

    smear cursor

---
+++ nvim/.config/nvim/lua/plugins/smear-cursor.lua
@@ -0,0 +1,14 @@
+return {
+  "sphamba/smear-cursor.nvim",
+  opts = {
+    smear_between_buffers = true,
+    smear_between_neighbor_lines = true,
+    scroll_buffer_space = true,
+    smear_insert_mode = true,
+    stiffness = 0.65,
+    trailing_stiffness = 0.35,
+    damping = 0.7,
+    never_draw_over_target = true,
+    hide_target_hack = true,
+  },
+}
