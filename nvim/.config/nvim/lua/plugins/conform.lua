d57856f07d23e98ddb2a7190d022cd287f66cac4
Juan Diego <juandiegomariscal@gmail.com>
2026-08-30 14:59:12 +0200

    quitamos prettier de js

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -2,11 +2,24 @@ return {
   "stevearc/conform.nvim",
   opts = function(_, opts)
     opts.formatters_by_ft = opts.formatters_by_ft or {}
-    opts.formatters_by_ft.javascript = { "prettier" }
-    opts.formatters_by_ft.typescript = { "prettier" }
-    opts.formatters_by_ft.html = { "prettier" }
-    opts.formatters_by_ft.css = { "prettier" }
-    opts.formatters_by_ft.json = { "prettier" }
+    for _, ft in ipairs({
+      "css",
+      "graphql",
+      "handlebars",
+      "html",
+      "javascript",
+      "javascriptreact",
+      "json",
+      "jsonc",
+      "less",
+      "scss",
+      "typescript",
+      "typescriptreact",
+      "vue",
+      "yaml",
+    }) do
+      opts.formatters_by_ft[ft] = { lsp_format = "never" }
+    end
     opts.formatters_by_ft.python = { "ruff", "black" }
     opts.formatters_by_ft.lua = { "stylua" }
     opts.formatters_by_ft.sql = { "sqlfmt" }
