f21e7b2fee692e28c26c7e470aa75656fa9aab3e
Juan Diego <juandiegomariscal@gmail.com>
2026-08-30 12:46:37 +0200

    separamos scripts de instalacion

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -2,14 +2,14 @@
 sudo dnf update -y
 
 # utilidades
-sudo dnf install pipx unzip xclip yq jq fzf stow eza tldr fastfetch micro stow btop brightnessctl fd -y
+sudo dnf install pipx unzip xclip yq jq fzf stow eza tldr fastfetch micro stow btop brightnessctl fd curl wget -y
 
 # hyprland
 sudo dnf copr enable sdegler/hyprland -y
 sudo dnf install hyprland hyprpaper hyprlock hypridle hyprsunset -y
 
 # niri
-sudo dnf install niri -y
+#sudo dnf install niri -y
 
 # noctalia
 sudo dnf install noctalia -y
@@ -23,68 +23,10 @@ curl -sS https://starship.rs/install.sh | sh -s -- --yes
 # zoxide
 curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
 
-# peticiones http
-sudo dnf install curl wget httpie -y
-
-#sql
-sudo dnf install sqlite3 sqlitebrowser -y
-
-# vscode
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc &&
-  echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo >/dev/null
-dnf check-update &&
-  sudo dnf install code -y
-
 # Nerd fonts
 wget -q https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip
 unzip -o JetBrainsMono.zip -d ~/.local/share/fonts
 fc-cache -f -v
-
-# gh cli
-sudo dnf install dnf5-plugins -y
-sudo dnf config-manager addrepo --from-repofile=https://cli.github.com/packages/rpm/gh-cli.repo -y
-sudo dnf install gh --repo gh-cli -y
-
-# entorno javascript
-sudo dnf install npm nodejs -y
-curl -fsSL https://bun.sh/install | bash
-
-# go
-sudo dnf install -y golang
-
-# uv
-curl -LsSf https://astral.sh/uv/install.sh | sh
-
-# java
-sudo dnf install java-latest-openjdk -y
-
-# docker
-sudo dnf remove docker \
-  docker-client \
-  docker-client-latest \
-  docker-common \
-  docker-latest \
-  docker-latest-logrotate \
-  docker-logrotate \
-  docker-selinux \
-  docker-engine-selinux \
-  docker-engine -y
-
-sudo dnf config-manager addrepo --from-repofile https://download.docker.com/linux/fedora/docker-ce.repo -y
-sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-sudo systemctl enable --now docker
-sudo groupadd -f docker
-sudo usermod -aG docker $USER
-
-# lazydocker
-curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
-
-# lazygit
-sudo dnf copr enable dejan/lazygit -y
-sudo dnf install lazygit -y
-
-#neovim
-sudo dnf install nvim -y
 
 #tetro-tui
 URL="https://github.com/Strophox/tetro-tui/releases/download/v3.1.0/tetro-tui_v3.1_x86_64-unknown-linux-gnu.zip"
@@ -96,21 +38,10 @@ sudo mv "$TMP_DIR/tetro-tui" /usr/local/bin/tetro-tui
 
 rm -rf "$TMP_DIR"
 
-# csvkit
-pipx install csvkit
-
 # lsoff
 curl -sL "$(curl -s https://api.github.com/repos/yutat23/lsoff/releases/latest | grep browser_download_url | grep linux-amd64 | cut -d '"' -f 4)" -o /tmp/lsoff
 chmod +x /tmp/lsoff
 sudo mv /tmp/lsoff /usr/local/bin/lsoff
-
-# try
-sudo dnf install ruby -y
-echo 'gem: -n ~/.local/bin' >>~/.gemrc
-gem install try-cli --user-install
-
-# opencode
-curl -fsSL https://opencode.ai/install | bash
 
 # Codecs (RPM Fusion + ffmpeg completo para mkv, h264, hevc, etc)
 sudo dnf install \
--- scripts/dev-tools.sh
+++ scripts/dev-tools.sh
@@ -1,1 +1,68 @@
 sudo dnf instal -y pgcli
+sudo dnf install -y httpie
+
+#sql
+sudo dnf install sqlite3 sqlitebrowser -y
+
+# vscode
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc &&
+  echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo >/dev/null
+dnf check-update &&
+  sudo dnf install code -y
+
+# gh cli
+sudo dnf install dnf5-plugins -y
+sudo dnf config-manager addrepo --from-repofile=https://cli.github.com/packages/rpm/gh-cli.repo -y
+sudo dnf install gh --repo gh-cli -y
+
+# entorno javascript
+sudo dnf install npm nodejs -y
+curl -fsSL https://bun.sh/install | bash
+
+# csvkit
+pipx install csvkit
+
+# go
+sudo dnf install -y golang
+
+# uv
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
+# java
+sudo dnf install java-latest-openjdk -y
+
+# lazydocker
+curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
+
+# lazygit
+sudo dnf copr enable dejan/lazygit -y
+sudo dnf install lazygit -y
+
+#neovim
+sudo dnf install nvim -y
+
+# docker
+sudo dnf remove docker \
+  docker-client \
+  docker-client-latest \
+  docker-common \
+  docker-latest \
+  docker-latest-logrotate \
+  docker-logrotate \
+  docker-selinux \
+  docker-engine-selinux \
+  docker-engine -y
+
+sudo dnf config-manager addrepo --from-repofile https://download.docker.com/linux/fedora/docker-ce.repo -y
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo systemctl enable --now docker
+sudo groupadd -f docker
+sudo usermod -aG docker $USER
+
+# try
+sudo dnf install ruby -y
+echo 'gem: -n ~/.local/bin' >>~/.gemrc
+gem install try-cli --user-install
+
+# opencode
+curl -fsSL https://opencode.ai/install | bash
