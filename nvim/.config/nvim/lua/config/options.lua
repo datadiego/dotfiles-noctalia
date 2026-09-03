1f50dc5695680e4d98805f982c6507842d8fdb0c
Juan Diego <juandiegomariscal@gmail.com>
2026-09-03 09:07:08 +0200

    quitamos el ftplugin de sql

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -6,3 +6,7 @@ local opt = vim.opt
 
 opt.sidescrolloff = 4  -- Columns of context (reducido de 8)
 opt.sidescroll = 1     -- Columns to scroll horizontally
+
+-- Disable default SQL ftplugin arrow key mappings that call sqlcomplete# functions
+-- (incompatible with blink.cmp/dadbod-completion used by LazyVim)
+vim.g.omni_sql_no_default_maps = true
