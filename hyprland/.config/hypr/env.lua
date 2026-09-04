058ab918776920ad8f1131ac35a04fa27040b14c
Juan Diego <juandiegomariscal@gmail.com>
2026-09-04 19:44:56 +0200

    variables para forzar backend grafico

--- hyprland/.config/hypr/env.lua
+++ hyprland/.config/hypr/env.lua
@@ -6,6 +6,8 @@ function M.setup(hl, terminal)
 	hl.env("XCURSOR_SIZE", "24")
 	hl.env("HYPRCURSOR_SIZE", "24")
 	hl.env("TERMINAL", terminal)
+	hl.env("GDK_BACKEND", "wayland")
+	hl.env("QT_QPA_PLATFORM", "wayland")
 end
 
 return M
