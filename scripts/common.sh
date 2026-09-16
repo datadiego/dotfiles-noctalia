dd775b098ae7456b1e97d33bcba67abe13d00930
juandiego <juandiegomariscal@gmail.com>
2026-09-16 02:27:40 +0200

    instalamos jc y wormhole

--- scripts/common.sh
+++ scripts/common.sh
@@ -7,6 +7,12 @@ sudo dnf install pipx unzip xclip yq jq fzf stow eza tldr fastfetch micro stow b
 # nushell
 sudo dnf install -y nu
 
+# jc
+pipx install jc
+
+# wormhole
+pipx install magic-wormhole
+
 # fish
 sudo dnf install -y fish
 
